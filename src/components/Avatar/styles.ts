5e9b1ae89ec7f35670b189d5d6c8620027b97892
Pedro Prenhaca <62861100+pedroprenhaca@users.noreply.github.com>
2023-01-09 23:02:49 -0300

    Update styles.ts

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -6,8 +6,7 @@ export const AvatarStyled = styled.div`
         width: 3rem;
         height: 3rem;
         border-radius:8px;
-        border: ${({hasBorder}) => hasBorder ? "4px solid #202024" : "none"};
-        outline: ${({hasBorder}) => hasBorder ? "2px solid #00875f" : "none"};
+        
     }
 
      
